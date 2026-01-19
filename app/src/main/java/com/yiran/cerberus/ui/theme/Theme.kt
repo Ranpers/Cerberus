08beafa9f353410f10c14250f0035053b310600d
Yiran <yi_ran@aliyun.com>
2026-01-20 01:41:14 +0800

    Remove unused Activity import from Theme.kt

--- app/src/main/java/com/yiran/cerberus/ui/theme/Theme.kt
+++ app/src/main/java/com/yiran/cerberus/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.yiran.cerberus.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
